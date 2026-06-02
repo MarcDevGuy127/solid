1e75ede7381685b2c52167b8734921f6b392c754
Marcelo Silva <Marcelo Silva@172.25.2.132>
2026-06-02 10:44:13 -0300

    application(Minecraft): add new player

--- exemplo_solid/src/application/Minecraft.java
+++ exemplo_solid/src/application/Minecraft.java
@@ -1,10 +1,23 @@
 package application;
 
+import entities.Creeper;
+import entities.Player;
+import entities.Skeleton;
+import interfaces.Dano;
+
 public class Minecraft {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
 
-	}
+        Player steve = new Player("Steve");
 
+        Dano creeper = new Creeper();
+        Dano skeleton = new Skeleton();
+
+        steve.atacar();
+        steve.curar();
+
+        creeper.atacar();
+        skeleton.atacar();
+    }
 }
