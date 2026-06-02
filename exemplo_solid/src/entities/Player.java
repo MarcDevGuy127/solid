9fad1bb3534ce7372b6f90d18279c905c2f728ed
Marcelo Silva <Marcelo Silva@172.25.2.132>
2026-06-02 10:37:48 -0300

    entities(Player): add interfaces implements

--- exemplo_solid/src/entities/Player.java
+++ exemplo_solid/src/entities/Player.java
@@ -1,5 +1,23 @@
 package entities;
 
-public class Player {
+import interfaces.Cura;
+import interfaces.Dano;
 
+public class Player implements Dano, Cura {
+
+    private String nome;
+
+    public Player(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public void atacar() {
+        System.out.println(nome + " atacou com espada!");
+    }
+
+    @Override
+    public void curar() {
+        System.out.println(nome + " comeu maçã dourada!");
+    }
 }
